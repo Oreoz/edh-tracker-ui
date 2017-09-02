f181ea28ad6dde9af57bd4864490e4dd5b40e659
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-09-02 00:23:56 -0400

    Moves match creation to a button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   store: Ember.inject.service(),
 
-  init() {
-    this._super(...arguments);
-
-    let match = this.get('store').createRecord('match', { name: 'Bogus Match' });
-    match.save();
+  actions: {
+    createMatch() {
+      let match = this.get('store').createRecord('match', { name: 'Bogus Match' });
+      match.save();
+    }
   }
 });
