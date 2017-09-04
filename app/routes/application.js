8bb91d734d63462e38c0edf73774f3a36cc2b575
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-09-03 20:30:53 -0400

    Hotfix
    - Fixes the issue where not having a session crashes the application;

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,10 +5,10 @@ export default Route.extend({
   session: service(),
 
   async beforeModel() {
-    await this.get('session').fetch();
+    await this.get('session').fetch().catch(() => {});
 
     let uid = this.get('session.currentUser.uid');
-    
+
     if (uid) {
       await this.get('store').query('profile', { uid: uid });
     }
